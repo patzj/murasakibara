061a0893c87573889acbf0a60e51bedd12ef4500
Pat Bautista <patzj006@gmail.com>
2017-10-25 09:17:45 +0800

    Able to handle updates

--- run.py
+++ run.py
@@ -8,11 +8,15 @@ try:
         sites = map(lambda x: x.strip(), list(fin))
 
     with open('/etc/hosts', 'a+') as fin:
+        content = list(fin)
         fin.write('\n')
         for site in sites:
-            fin.write('{} {}\n'.format(IP, site))
-            fin.write('{} www.{}\n'.format(IP, site))
-            fin.write('{} m.{}\n'.format(IP, site))
+            entries = ['{} {}\n'.format(IP, site),
+            '{} www.{}\n'.format(IP, site),
+            '{} m.{}\n'.format(IP, site)]
+            for entry in entries:
+                if content.count(entry) < 1:
+                    fin.write(entry)
 
     os.system('sudo service network-manager restart')
     print 'OK'
