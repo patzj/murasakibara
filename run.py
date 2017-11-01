baea179bceb51093701a84f714fb1e49f23b1dcc
Pat Bautista <patzj006@gmail.com>
2017-11-01 16:16:07 +0800

    Dynamic list

--- run.py
+++ run.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
 import os
+import socket
 
 try:
     IP = '127.0.0.1'
+    SCRIPT_COMMENT = '### murasakibara ###\n'
+
     with open('sites.txt', 'r') as fin:
         sites = map(lambda x: x.strip(), list(fin))
 
-    with open('/etc/hosts', 'a+') as fin:
+    with open('/etc/hosts', 'r') as fin:
         content = list(fin)
+
+        if SCRIPT_COMMENT in content:
+            i = content.index(SCRIPT_COMMENT)
+            content = content[:i]
+
+    with open('/etc/hosts', 'w') as fin:
+        for line in content:
+            fin.write(line)
+        fin.write(SCRIPT_COMMENT)
         fin.write('\n')
         for site in sites:
             entries = ['{} {}\n'.format(IP, site),
